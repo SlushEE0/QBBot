61e84856855470b6478952d9c56cb9f0eda8a580
SlushEE0 <101753528+SlushEE0@users.noreply.github.com>
2024-02-07 16:49:52 -0800

    chore: stripped it bare ;)

--- index.ts
+++ index.ts
@@ -1,1 +1,43 @@
-console.log("Hello via Bun!");
+import express from "express";
+import type {Request, Response} from "express"
+import {
+  InteractionType,
+  InteractionResponseType,
+  InteractionResponseFlags,
+  MessageComponentTypes,
+  ButtonStyleTypes
+} from "discord-interactions";
+
+import "dotenv/config";
+import { PORT } from "./utils/config";
+import { VerifyDiscordRequest } from "./utils/utils";
+
+const app = express();
+
+app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
+
+app.post("/api/interactions", async function (req: Request, res:Response) {
+  const { type, id, data } = req.body;
+
+  if (type === InteractionType.PING) {
+    return res.send({ type: InteractionResponseType.PONG });
+  }
+
+  if (type === InteractionType.APPLICATION_COMMAND) {
+    const { name } = data;
+
+    if (name === "test") {
+
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: JSON.stringify(req.body)
+        }
+      });
+    }
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Listening on port", PORT);
+});
